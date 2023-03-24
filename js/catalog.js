ebb8fee44f59bfd85276cd0508e6327069e66054
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-03-24 12:15:52 -0500

    Update catalog.js

--- js/catalog.js
+++ js/catalog.js
@@ -38,11 +38,11 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  // TODO: get the quantity
+  // TODO: using those, add one item to the Cart
   if(product){
 
   }
-  // TODO: get the quantity
-  // TODO: using those, add one item to the Cart
 }
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
